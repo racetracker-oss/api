a56306329737fb97d39d476474c4de2032c01e5a
Flórián Lovasi <lovasifloriian@outlook.hu>
2024-10-23 11:23:45 +0200

    install webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("node:path");
+const webpackNodeExternals = require("webpack-node-externals");
+const { NODE_ENV = "production" } = process.env;
+
+/**
+ * @type {import("webpack").Configuration}
+ * */
+module.exports = {
+  entry: "./src/main.ts",
+  mode: NODE_ENV,
+  watch: NODE_ENV === "development",
+  target: "node",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+  externals: [webpackNodeExternals()],
+};
