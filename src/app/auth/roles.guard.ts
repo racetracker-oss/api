655ded0bfd94fe45f4de9c2d43f4f3c0019c4989
Flórián Lovasi <lovasifloriian@outlook.hu>
2024-11-01 11:41:44 +0100

    roles guard

---
+++ src/app/auth/roles.guard.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/database";
+import type { Role } from "@prisma/client";
+import type { NextFunction, Request, Response } from "express";
+
+export const rolesGuard =
+  (roles: Role[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await prisma.user.findUnique({ where: { id: +req.user.sub } });
+    if (!user) {
+      return res.status(403).send("Forbidden");
+    }
+
+    if (roles.includes(user.role)) {
+      return next();
+    }
+
+    return res.status(403).send("Forbidden");
+  };
