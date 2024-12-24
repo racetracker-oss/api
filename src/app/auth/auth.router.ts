ff31ed1cec458071313974828e760fe4ab8e4127
Flórián Lovasi <lovasifloriian@outlook.hu>
2024-12-24 10:34:38 +0100

    add /refresh-token route

--- src/app/auth/auth.router.ts
+++ src/app/auth/auth.router.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
-import { signIn, signUp } from "./auth.controller";
+import { me, refreshToken, signIn, signUp } from "./auth.controller";
 import { signInSchema, signUpSchema } from "./schemas";
 import { validateBody } from "../common";
+import { rtStrategy } from "./strategies/rt.strategy";
+import { auth } from "./auth.middleware";
+import passport from "passport";
 const router = Router();
 
 //@ts-ignore
 router.post("/auth/sign-in", validateBody(signInSchema), signIn);
 router.post("/auth/sign-up", validateBody(signUpSchema), signUp);
+router.post(
+  "/auth/refresh-token",
+  passport.authenticate(rtStrategy, { session: false }),
+  refreshToken
+);
+router.get("/auth/me", auth, me);
 
 export { router as authRouter };
