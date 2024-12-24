698c91dd55445bd4d7fb5c3e5205272dae9a7738
Flórián Lovasi <lovasifloriian@outlook.hu>
2024-12-24 10:35:44 +0100

    add middlewares

--- src/configs.ts
+++ src/configs.ts
@@ -3,10 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import * as bodyparser from "body-parser";
 import morgan from "morgan";
+import { authErrorHandler } from "./app/auth/auth.middleware";
 
 export function initConfig(app: Express): void {
   app.use(cors({ origin: "*" }));
   app.use(cookieParser());
   app.use(bodyparser.urlencoded({ extended: true }));
+  app.use(bodyparser.json());
   app.use(morgan(":method :url :response-time ms"));
+  app.use(authErrorHandler);
 }
