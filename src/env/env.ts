c1db6a31760a64abcfc0c96bf514c6322e62569b
Flórián Lovasi <lovasifloriian@outlook.hu>
2024-12-24 10:29:03 +0100

    add refresh token secret

--- src/env/env.ts
+++ src/env/env.ts
@@ -4,12 +4,14 @@ export const envSchema = z.object({
   DATABASE_URL: z.string(),
   ACCESS_TOKEN_SECRET: z.string(),
   PORT: z.number().optional(),
+  REFRESH_TOKEN_SECRET: z.string(),
 });
 
 const processEnv: z.infer<typeof envSchema> = {
   DATABASE_URL: process.env.DATABASE_URL,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   PORT: Number.parseInt(process.env.PORT),
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
 };
 
 const result = envSchema.safeParse(processEnv);
