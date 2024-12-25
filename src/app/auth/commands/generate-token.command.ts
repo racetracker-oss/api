f711d55df8ddae25c6cf24f717fe6d5542c9af23
Flórián Lovasi <lovasifloriian@outlook.hu>
2024-12-25 11:03:48 +0100

    use environment

--- src/app/auth/commands/generate-token.command.ts
+++ src/app/auth/commands/generate-token.command.ts
@@ -4,8 +4,8 @@ import jwt from "jsonwebtoken";
 
 export const generateTokens = (payload: JwtPayload) => {
   const [accessToken, refreshToken] = [
-    jwt.sign(payload, env.ACCESS_TOKEN_SECRET, { expiresIn: "1m" }),
-    jwt.sign(payload, env.REFRESH_TOKEN_SECRET, { expiresIn: "10m" }),
+    jwt.sign(payload, env.ACCESS_TOKEN_SECRET, { expiresIn: env.ACCESS_TOKEN_EXPIRES_IN }),
+    jwt.sign(payload, env.REFRESH_TOKEN_SECRET, { expiresIn: env.REFRESH_TOKEN_EXPIRES_IN }),
   ];
 
   return [accessToken, refreshToken];
