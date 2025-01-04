b76367cb6ccb18e235c3efcff25407ff4ffb9869
Flórián Lovasi <lovasifloriian@outlook.hu>
2025-01-04 17:40:20 +0100

    fix types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,15 @@
 import "express";
 import type { envSchema } from "./env/env";
 import type { User as PrismaUser } from "@prisma/client";
+
 declare global {
   namespace NodeJS {
     interface ProcessEnv extends z.infer<typeof envSchema> {}
   }
   namespace Express {
-    interface User extends PrismaUser {}
+    interface Request {
+      user?: PrismaUser;
+    }
   }
 }
 
