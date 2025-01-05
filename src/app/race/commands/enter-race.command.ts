26d99dae49ef9bd2dacb997002c43f61eaf30ed3
Flórián Lovasi <lovasifloriian@outlook.hu>
2025-01-05 12:46:18 +0100

    handling race leave, join

--- src/app/race/commands/enter-race.command.ts
+++ src/app/race/commands/enter-race.command.ts
@@ -1,19 +1,46 @@
 import { prisma } from "@/database";
 import type { User } from "@prisma/client";
-import { RaceNotFoundError } from "../errors";
+import { CanNotJoinRaceError, RaceNotFoundError } from "../errors";
 
 export const enterRace = async (user: User, raceCode: string) => {
+  const today = new Date();
   const race = await prisma.race.findUnique({
     where: {
       joinCode: raceCode,
-    },
-    select: {
-      id: true,
     },
   });
 
   if (!race) {
     throw new RaceNotFoundError();
+  }
+
+  if (today > race.joinUntil) {
+    throw new CanNotJoinRaceError(
+      "Can not join to the race. Reason: Race join time is over."
+    );
+  }
+
+  if (today > race.startDate) {
+    throw new CanNotJoinRaceError(
+      "Can not join to the race. Reason: Race has already started."
+    );
+  }
+
+  const isUserAlreadyInRace = await prisma.user.findFirst({
+    where: {
+      id: user.id,
+      Race: {
+        some: {
+          joinCode: raceCode,
+        },
+      },
+    },
+  });
+
+  if (isUserAlreadyInRace) {
+    throw new CanNotJoinRaceError(
+      "Can not join to the race. Reason: Already joined"
+    );
   }
 
   return await prisma.user.update({
--- src/app/race/commands/leave-race.command.ts
+++ src/app/race/commands/leave-race.command.ts
@@ -1,11 +1,28 @@
 import type { User } from "@prisma/client";
 import { getRace } from "./get-race.command";
-import { RaceNotFoundError } from "../errors";
+import { CanNotLeaveRaceError, RaceNotFoundError } from "../errors";
 import { prisma } from "@/database";
 
 export const handleLeaveRace = async (user: User, code: string) => {
   const race = await getRace.byCode(code);
   if (!race) throw new RaceNotFoundError();
+
+  const isUserAlreadyInRace = await prisma.user.findFirst({
+    where: {
+      id: user.id,
+      Race: {
+        some: {
+          joinCode: code,
+        },
+      },
+    },
+  });
+
+  if (!isUserAlreadyInRace) {
+    throw new CanNotLeaveRaceError(
+      "Can not leave the race. Reason: Not joined"
+    );
+  }
 
   const leaveRace = await prisma.user.update({
     where: { id: user.id },
